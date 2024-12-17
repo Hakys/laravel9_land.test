700a9659e613056c86f3bea73377a53bc9e1a4c8
Hakys <hakyss@gmail.com>
2024-12-17 13:41:15 +0100

    no cache

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,10 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content="laravel9 bootstrap5" />
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
+    <meta name="description" content="App Diabla Roja Web Services" />
     <meta name="author" content="by Hakys" />
     <title>@yield('title', ' - Diabla Roja')</title>
     <!-- Favicon-->
