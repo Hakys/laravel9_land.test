02a81ae17498a2fad267e5a3cbc78b28d1519223
Hakys <hakyss@gmail.com>
2025-01-03 14:18:18 +0100

    sas rebuild 2

--- vite.config.js
+++ vite.config.js
@@ -11,11 +11,7 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    resolve: {
-        alias: {
-            '@': '/resources/js',
-        },
-    },
+
     css: {
         preprocessorOptions: {
             scss: {
