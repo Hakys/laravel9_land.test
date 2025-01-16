6f7c4d605c044ab906bf8ddd97da858a55b4e6e4
Hakys <hakyss@gmail.com>
2025-01-16 14:39:41 +0100

    pre build 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,33 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import path from 'path';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
-    resolve: {
-        alias: {
-            '@': '/resources/js',
+export default defineConfig(async () => {
+    const laravelPlugin = (await import('laravel-vite-plugin')).default;
+
+    return {
+        plugins: [
+            laravelPlugin({
+                input: 'resources/js/app.js',
+                refresh: true,
+            }),
+        ],
+        resolve: {
+            alias: {
+                '@': '/resources/js',
+            },
         },
-    },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: `@import "/node_modules/bootstrap/dist/css/bootstrap.css";`
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: `@import "node_modules/bootstrap/scss/bootstrap";`
+                }
             }
-        }
-    },
-    build: {
-        manifest: true,
-        rollupOptions: {
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
         },
-    },
+        build: {
+            manifest: true,
+            rollupOptions: {
+                input: 'resources/js/app.js',
+            },
+        },
+    };
 });
